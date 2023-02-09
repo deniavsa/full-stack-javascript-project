a0acf1c8ec3d130eaec838aac718ae11de8f97c9
Denilson <denilson.avsantos@hotmail.com>
2023-02-09 15:43:37 -0300

    feat: CRUD items

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello world!')
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors"; 
+import helmet from "helmet";
+import { itemsRouter } from "./router/items.route";
+
+dotenv.config(); 
+
+if (!process.env.PORT) {
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/menu/items", itemsRouter);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
